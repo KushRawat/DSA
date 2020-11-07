a1069c7113bb4510e4dd82de9b18cad4cb6a2464
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2020-11-07 23:06:04 +0530

    correcting faster algorithm

--- I/1/maxPairwise.py
+++ I/1/maxPairwise.py
@@ -4,18 +4,22 @@ n = int(input())
 
 digits = [int(x) for x in input().split()]
 
-index1 = 0
-for i in range(1, n):
-    if digits[i] > digits[index1]:
-        index1 = i
+def MaxPairwiseProductFast(n, digits):
 
-if index1 == 0:         # handling descending digits 
-    index2 = 1
+    index1 = 0
+    for i in range(1, n):
+        if digits[i] > digits[index1]:
+            index1 = i
 
-for i in range(1, n):
-    if digits[i] != digits[index1] and digits[i] > digits[index2]:
-        index2 = i
+    if index1 == 0:         # handling descending digits 
+        index2 = 1
+    else:
+        index2 = 0
 
-print(digits[index1] * digits[index2])
+    for i in range(1, n):
+        if digits[i] != digits[index1] and digits[i] > digits[index2]:
+            index2 = i
 
-    
+    return (digits[index1] * digits[index2])
+
+print(MaxPairwiseProductFast(n, digits))        
