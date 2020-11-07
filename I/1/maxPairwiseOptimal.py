6c44e835a727f31c7276d015b80ed3b447c02188
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2020-11-07 23:40:05 +0530

    optimal solution

---
+++ I/1/maxPairwiseOptimal.py
@@ -0,0 +1,9 @@
+def maxPairwiseOptimal(digits):
+    
+    digits.sort()
+    return digits[-1] * digits[-2]
+
+n = int(input())
+digits = [int(x) for x in input().split()]
+
+print(maxPairwiseOptimal(digits))
