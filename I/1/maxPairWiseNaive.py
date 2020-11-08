6307177a2914469361ce19adadd431b764a57444
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2020-11-09 01:42:53 +0530

    naive solution

---
+++ I/1/maxPairWiseNaive.py
@@ -0,0 +1,14 @@
+def maxPairWiseNaive(digits, n):
+
+    product = 0
+
+    for i in range(n):
+        for j in range(i + 1, n):
+            product = max(product, digits[i] * digits[j])
+
+    return product
+
+n = int(input())
+digits = [int(x) for x in input().split()]
+
+print(maxPairWiseNaive(digits, n))
