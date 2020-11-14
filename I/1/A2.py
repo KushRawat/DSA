943aa565ecd5aeb13022ee010b7b260f23e581b0
Kush Rawat <50131042+KushRawat@users.noreply.github.com>
2020-11-14 22:39:20 +0530

    stress Testing algorithm 2

---
+++ I/1/A2.py
@@ -0,0 +1,10 @@
+import random
+import sys
+
+n = int(sys.argv[1])
+mySeed = int(sys.argv[2])   
+random.seed(mySeed)         # used to customize the initial random number which otherwise is based on the current time 
+
+print(n)
+
+print('-'.join([str(random.randint(1,1000)) for i in range (n)]))
